1d2a211e0dcf289e23eb2ff2542eacd9c8415edd
Evgeniy Naumov <63817948+evgenii020304@users.noreply.github.com>
2023-02-26 17:54:37 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -11,37 +11,37 @@ double pown(double value, uint16_t n) {
 }
 
 uint64_t fact(uint16_t n) {
-	if ((n == 1) || (n == 0)) {
-		return 1;
-	} else {
-		return n * fact(n - 1);
-	}
+  if ((n == 1) || (n == 0)) {
+    return 1;
+  } else {
+    return n * fact(n - 1);
+  }
 }
 
 double calcItem(double x, uint16_t n) {
-	return (pown(x, n) / fact(n));
+  return (pown(x, n) / fact(n));
 }
 
 double expn(double x, uint16_t count) {
-	double sum = 1.0;
-	for (count; count > 0; count--) {
-		sum += calcItem(x, count);
-	}
-	return sum;
+  double sum = 1.0;
+  for (count; count > 0; count--) {
+    sum += calcItem(x, count);
+  }
+  return sum;
 }
 
 double sinn(double x, uint16_t count) {
-	double sum = 0;
-	for (count; count > 0; count--) {
-		sum += pown(-1, count-1) * calcItem(x, 2*count-1);
-	}
-	return sum;
+  double sum = 0;
+  for (count; count > 0; count--) {
+    sum += pown(-1, count-1) * calcItem(x, 2*count-1);
+  }
+  return sum;
 }
 
 double cosn(double x, uint16_t count) {
-	double sum = 0;
-	for (count; count > 0; count--) {
-		sum += pown(-1, count-1) * calcItem(x, 2*count-2);
-	}
-	return sum;
+  double sum = 0;
+  for (count; count > 0; count--) {
+    sum += pown(-1, count-1) * calcItem(x, 2*count-2);
+  }
+  return sum;
 }
